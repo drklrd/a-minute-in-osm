8c94e735ae4c5a715bff717f5e52e0886b7cd531
drklrd <srvbhattarai@gmail.com>
2019-01-08 20:29:01 +0545

    fix margin

--- client/build/precache-manifest.970dfcc269781051bdf4f1761df16cae.js
+++ client/build/precache-manifest.970dfcc269781051bdf4f1761df16cae.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "e5b58d2e3dd2a1c39588",
-    "url": "/static/js/main.e5b58d2e.chunk.js"
+    "revision": "a9cbf33285556789f858",
+    "url": "/static/js/main.a9cbf332.chunk.js"
   },
   {
     "revision": "f5d67a810d6a63dbdfe4",
     "url": "/static/js/1.f5d67a81.chunk.js"
   },
   {
-    "revision": "e5b58d2e3dd2a1c39588",
-    "url": "/static/css/main.93ebab74.chunk.css"
+    "revision": "a9cbf33285556789f858",
+    "url": "/static/css/main.ccf87f1d.chunk.css"
   },
   {
     "revision": "f5d67a810d6a63dbdfe4",
     "url": "/static/css/1.df394713.chunk.css"
   },
   {
-    "revision": "0b1fbdae04fb4a252674b5d2d55a947f",
+    "revision": "e06ae9682f0fe408a85989d475c0423b",
     "url": "/index.html"
   }
 ];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ba67055b872d99cb22d0e98adc1bed5.js"
+  "/precache-manifest.970dfcc269781051bdf4f1761df16cae.js"
 );
 
 workbox.clientsClaim();
