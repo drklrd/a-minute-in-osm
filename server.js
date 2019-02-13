3bb23ce495aa2a12d222d80bd00764c3d4cc5a75
drklrd <srvbhattarai@gmail.com>
2019-02-13 16:40:56 +0545

    add twitte

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ const app = express();
 // 	cronJob.fireOnTick();
 // }
 
+const Twit = require('twit');
+let T = null;
+if(process && process.env && process.env.consumer_key){
+	T = new Twit({
+	  consumer_key:         process.env.consumer_key,
+	  consumer_secret:      process.env.consumer_secret,
+	  access_token:         process.env.access_token,
+	  access_token_secret:  process.env.access_token_secret,
+	  timeout_ms:           60 * 1000,
+	});
+}
+
 app.use(express.static('client/build'));
 
 app.use(function(req, res, next) {
@@ -36,6 +48,44 @@ app.get("/api/keepalive", (req, res, next) => {
 			});
 		})
  
+});
+
+app.get("/api/twitte/hour", (req, res, next) => {
+ 	const osmpullscript = require("./osmpullscript");
+ 	osmpullscript.pull()
+ 		.then((done)=>{
+ 			const stats = JSON.parse(fs.readFileSync(`./stats.json`, "utf8"));
+ 			// Post a tweet
+ 			if(stats.users.length && stats.wayBuildings){
+ 				const tweetMessage = `Within this last minute in OSM, ${stats.users.length} users contributed in creating ${stats.changesets.length} changesets. ${stats.wayBuildings} buildings were also created. In total, ${stats.createnode} nodes, ${stats.createway} ways and ${stats.createrelation} realations were created. See more at https://a-minute-in-osm.herokuapp.com`;
+ 				T.post('statuses/update', { status: tweetMessage },
+ 				  (err, data, response) => {
+ 				    if(err){
+ 				    	res.json({
+ 				    		err : 1,
+ 				    		message : err
+ 				    	})
+ 				    }
+ 				    res.json({
+ 				    	success:1,
+ 				    	message : "Successfully twitted!"
+ 				    })
+ 				  }
+ 				)
+ 			}else{
+ 				res.json({
+ 					success:1,
+ 					message : "Noting to tweet"
+ 				})
+ 			}
+ 		})
+ 		.catch((err)=>{
+ 			console.log("@@@@",err)
+ 			res.json({
+ 				err : 1,
+ 				message : err
+ 			})
+ 		})
 });
 
 
